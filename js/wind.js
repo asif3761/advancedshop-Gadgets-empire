c565f5c9f8cf100772133e1a4e63665f65bc65dc
asif3761 <asifuzzaman3761@gmail.com>
2026-07-30 20:01:02 +0600

    Create wind.js

---
+++ js/wind.js
@@ -0,0 +1,17 @@
+// Gadget's Empire Wind Effect
+
+const wind = document.createElement("div");
+
+wind.className = "wind-layer";
+
+document.body.appendChild(wind);
+
+let strength = 0;
+
+document.addEventListener("mousemove", (e) => {
+
+    strength = (e.clientX / window.innerWidth) * 30;
+
+    wind.style.opacity = 0.3 + strength / 100;
+
+});
